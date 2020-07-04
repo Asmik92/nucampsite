cf76ff39e1f5ecd6642d808603517a32a2b4dd70
Asmik Arustamyan <asmikbarca@gmail.com>
2020-07-04 09:17:17 -0700

    first commit

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+Class CampsiteInfo extends Component {
+    renderCampsite(campsite) {
+        return (
+            <div className="row">
+                </div>
+                
+        );
+    }
+}
+export default CampsiteInfo;
